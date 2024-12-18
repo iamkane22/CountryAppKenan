28d0ec9cbcaede38e4e0cd40243e725a9acc39e6
iamkane22 <manaflikenan222@gmail.com>
2024-12-18 18:58:19 +0400

    uyguyg
    ubu9b

--- Presentation/Core/BaseViewController.swift
+++ Presentation/Core/BaseViewController.swift
@@ -1,24 +1,24 @@
-//
-//  BaseViewController.swift
-//  CountryAppKenan
-//
-//  Created by Kenan on 14.12.24.
-//
+    //
+    //  BaseViewController.swift
+    //  CountryAppKenan
+    //
+    //  Created by Kenan on 14.12.24.
+    //
 
-import UIKit
+    import UIKit
 
-class BaseViewController: UIViewController {
+    class BaseViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        configureView()
-        configureConstraint()
-        configureTargets()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .systemBackground
+            configureView()
+            configureConstraint()
+            configureTargets()
+        }
+        
+        open func configureView() {}
+        open func configureConstraint() {}
+        open func configureTargets() {}
+
     }
-    
-    open func configureView() {}
-    open func configureConstraint() {}
-    open func configureTargets() {}
-
-}
